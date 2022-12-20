d8c62dc58ddb03e684e764cdfbcacfbe2c65e381
succynice <succynice@gmail.com>
2022-12-20 20:43:15 +0000

    Block Scope

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,11 @@
+export default function taskBlock(trueOrFalse) {
+  const task11 = false;
+  const task22 = true;
+
+  if (trueOrFalse) {
+    const task = true; // eslint-disable-line no-unused-vars
+    const task2 = false; // eslint-disable-line no-unused-vars
+  }
+
+  return [task11, task22];
+}
